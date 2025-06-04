4930c29d69550f50b6714799a5e8c23773c10015
Mateus Oliveira <warface01031999@gmail.com>
2025-06-04 09:42:44 -0300

    Disable remote module

--- src/main/index.js
+++ src/main/index.js
@@ -9,7 +9,8 @@ function createWindow () {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: false,
-      contextIsolation: true
+      contextIsolation: true,
+      enableRemoteModule: false
     }
   });
 
