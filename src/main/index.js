fe13e9217c22cfdeeace7fc4f9bb17c49b5a9727
Mateus Oliveira <warface01031999@gmail.com>
2025-06-04 15:59:01 -0300

    test: ensure menu removed

--- src/main/index.js
+++ src/main/index.js
@@ -17,6 +17,7 @@ function createWindow () {
       enableRemoteModule: false
     }
   });
+  win.removeMenu();
 
   if (process.env.NODE_ENV === 'development') {
     win.loadURL('http://localhost:5173');
