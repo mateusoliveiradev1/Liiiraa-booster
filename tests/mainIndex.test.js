c4a181918181c968f0bc264834d839fea7aeb146
Mateus Oliveira <warface01031999@gmail.com>
2025-06-04 17:56:06 -0300

    test: verify BrowserWindow security options

--- tests/mainIndex.test.js
+++ tests/mainIndex.test.js
@@ -2,13 +2,17 @@
 
 jest.mock('electron', () => {
   const removeMenu = jest.fn();
-  function BrowserWindow() {}
+  let lastOptions;
+  function BrowserWindow(options) {
+    lastOptions = options;
+  }
   BrowserWindow.prototype.loadURL = jest.fn();
   BrowserWindow.prototype.loadFile = jest.fn();
   BrowserWindow.prototype.removeMenu = removeMenu;
 
   return {
     __removeMenu: removeMenu,
+    __browserWindowOptions: () => lastOptions,
     app: { whenReady: jest.fn(() => Promise.resolve()), on: jest.fn() },
     BrowserWindow,
     ipcMain: { handle: jest.fn() },
@@ -20,9 +24,20 @@ require('@testing-library/jest-dom');
 
 require('../src/main/index.js');
 
-const { __removeMenu } = require('electron');
+const { __removeMenu, __browserWindowOptions } = require('electron');
 
- test('removeMenu called on BrowserWindow', async () => {
+test('removeMenu called on BrowserWindow', async () => {
   await Promise.resolve();
   expect(__removeMenu).toHaveBeenCalled();
+});
+
+test('BrowserWindow created with secure preferences', async () => {
+  await Promise.resolve();
+  const opts = __browserWindowOptions();
+  expect(opts.webPreferences).toMatchObject({
+    nodeIntegration: false,
+    contextIsolation: true,
+    enableRemoteModule: false,
+    sandbox: true
+  });
 });
